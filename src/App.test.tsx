87952dd3a1b99dc02cc81903198a8b1b6b769302
nirmala <nirmala@surf.net.np>
2020-09-04 20:52:38 +0200

    Add random number generator feature

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders button to roll dice', () => {
+  const { getByText, getByTestId } = render(<App />);
+  const buttonElement = getByText(/Roll D20/i)  
+  expect(buttonElement).toBeInTheDocument();
+  expect(getByTestId('displayD20')).toHaveTextContent('')
+  userEvent.click(buttonElement)
+  expect(getByTestId('displayD20')).toHaveTextContent(/^\d+$/);
+  
+
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function randomNumberGenerator(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 function App() {
+  const [number, setNumber] = React.useState('')  
+    
+  const handleChange = () => {
+    const randValue = randomNumberGenerator(0, 20)
+    setNumber(randValue.toString())
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p data-testid="displayD20">{number}</p>
+        <button onClick={handleChange}>Roll D20</button>        
       </header>
     </div>
   );
