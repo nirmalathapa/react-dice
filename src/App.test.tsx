2b306eeb2551097fb1ae5a01ef982bb9d4a87be0
nirmala <nirmala@surf.net.np>
2020-09-05 18:46:42 +0200

    Format and lint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders button to roll dice', () => {
+test("renders button to roll dice", () => {
   const { getByText, getByTestId } = render(<App />);
-  const buttonElement = getByText(/Roll D20/i)  
+  const buttonElement = getByText(/Roll D20/i);
   expect(buttonElement).toBeInTheDocument();
-  expect(getByTestId('displayD20')).toHaveTextContent('')
-  userEvent.click(buttonElement)
-  expect(getByTestId('displayD20')).toHaveTextContent(/^\d+$/);
-  
-
+  expect(getByTestId("displayD20")).toHaveTextContent("");
+  userEvent.click(buttonElement);
+  expect(getByTestId("displayD20")).toHaveTextContent(/^\d+$/);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function randomNumberGenerator(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function App() {
-  const [number, setNumber] = React.useState('')  
-    
+  const [number, setNumber] = React.useState("");
+
   const handleChange = () => {
-    const randValue = randomNumberGenerator(0, 20)
-    setNumber(randValue.toString())
-  }
+    const randValue = randomNumberGenerator(0, 20);
+    setNumber(randValue.toString());
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <p data-testid="displayD20">{number}</p>
-        <button onClick={handleChange}>Roll D20</button>        
+        <button onClick={handleChange}>Roll D20</button>
       </header>
     </div>
   );
